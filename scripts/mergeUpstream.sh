6b7690f44e755d747d582d5caa8e92a7eac3b6b6
Indhi Rousseau <contact@thekinrar.fr>
2020-08-02 15:47:04 +0200

    Updated Upstream (BungeeCord/Waterfall)
    Upstream has released updates that appears to apply and compile correctly.
    
    BungeeCord Changes:
    15b51413 #2908: Don't frame packets for dead connections
    a0f9333a Bump version to 1.16-R0.4-SNAPSHOT
    287e28a7 Release 1.16-R0.3
    c1522ab9 #2909: Don't serialise as array for single element contents
    0af4bfdb #2905: HoverEvent getValue compat method
    94c4fcba Bump version to 1.16-R0.3-SNAPSHOT
    a99f62f6 Release 1.16-R0.2
    fd4864d4 #2899: Various chat API stability updates
    c5610a6a #2897: Add params of recursive call of ChatComponent transform
    bcc3460d #2893: Refactored Hover Code for Maintainability
    4794fccf #2888: Correct font check in BaseComponent
    637e7e93 #2884: Mojangson in hover events cannot be parsed
    2e4b08e5 #2878: Null check hover event before transform
    a64c34d2 #2875: Add the MessageRaw channel
    1d40b8a8 #2866: Add support for contents in Hover Event
    26f538d1 Clear tab list on server change
    afcfac31 #2873: BossBar stays after switching servers
    6cff5a95 #2864: Add release maven profile
    e21b0b37 #2870: Add rgb values to all colors
    d65ee874 #2865: Bump actions/checkout version
    c803f42a #2867: Add font setter to ComponentBuilder
    3409fe6d Clear scoreboards and bossbars on new style server switch
    4786c098 Bump version to 1.16-R0.2-SNAPSHOT
    a7180850 Release 1.16-R0.1
    e1084bd9 #2861: Improve dimension changing support in 1.16.x
    c5f839c9 #2862: Minecraft 1.16.1 support
    87cb3b90 Improve license info in pom.xml
    198004da Add release maven repository
    b41e9be4 Constrain dimension to String|Integer
    739b496b #2858: ChatColor#ordinal is missing
    75af27ac Fix missing static on ChatColor.values
    d0fd673b Minecraft 1.16 support + RGB ChatColor preview
    2f54c943 #2786: Should first peek keepalive queue
    67c2dfd8 #2794: connection throttle race condition
    eeb3c6d3 #2811: Shorten "Cannot request protocol" exception
    727281e6 Make all BaseComponent.duplicate methods specific
    4e99a325 #2808: TextComponent.duplicate should return TextComponent
    3373eb86 #2834: Add support for long array NBT tag
    b85df4f2 #2830: CommandSend - replace iterator-while with for-each
    855d1525 #2829: Fix action bar messages on pre 1.11 versions
    a1969b2f #2823: Update Netty to 4.1.49.Final
    b91d4d30 #2820: Sending a player via /send prints incorrect message
    aa66633d #2821: Add support for brigadier:long argument type
    
    Waterfall Changes:
    a619858 Updated Upstream (BungeeCord) (#549)
    6f55959 Apply SpigotMC/BungeeCord#2908 (#546)
    7ddd12d Fix compile and move version changes out of the wrong patch
    7737b0f Updated Upstream (BungeeCord)
    4cd9150 Updated Upstream (BungeeCord)
    132fd5d Updated Upstream (BungeeCord)
    f6b6c61 Updated Upstream (BungeeCord)
    8fb8b6c Updated Upstream (BungeeCord)
    d2dc905 Updated Upstream and Fix switching servers with disable metadata rewrite (#523)
    7ec32c4 Temp 1.16.1 proto support
    3781053 Fix POM
    7bb1088 Updated Upstream (BungeeCord)
    360fdcf Updated Upstream (BungeeCord)
    61508bf fix derp in protocol mapping again
    52291f0 Fix Entity effect packet mappings
    d61b93a Updated Upstream (BungeeCord)
    b3296ad Disable metadata rewriting for 1.16 (#520)
    29b6e01 Preliminary 1.16 support (#494)
    3a8916d Remove version from brand info (Fixes #517)
    33c73f9 Use vanilla maximum for tab completion packets (Fixes #512)
    717477a Updated Upstream (BungeeCord)
    26f574b Cache some more common exceptions
    ffe3d76 Updated Upstream (BungeeCord)
    2cab85c Don't intern scoreboard player names
    331ebbf Add exception reporting event (#488)
    a469ee5 Updated Upstream (BungeeCord)
    351a94d Use configured timeout value in UserConnection#connect (#490)

--- scripts/mergeUpstream.sh
+++ scripts/mergeUpstream.sh
@@ -4,13 +4,14 @@ PS1="$"
 basedir=`pwd`
 
 function update {
-    cd "$basedir/$1"
+    cd "$basedir/work/$1"
     git fetch && git reset --hard origin/master
-    cd "$basedir/$1/.."
+    cd "$basedir/work/$1/.."
     git add $1
 }
 
 update BungeeCord
+update Waterfall
 
 # Update submodules
 git submodule update --recursive
--- scripts/upstreamCommit.sh
+++ scripts/upstreamCommit.sh
@@ -7,7 +7,8 @@ function changelog() {
     base=$(git ls-tree HEAD $1  | cut -d' ' -f3 | cut -f1)
     cd $1 && git log --oneline ${base}...HEAD
 }
-bungee=$(changelog BungeeCord)
+bungee=$(changelog work/BungeeCord)
+waterfall=$(changelog work/Waterfall)
 
 updated=""
 logsuffix=""
@@ -15,7 +16,11 @@ if [ ! -z "$bungee" ]; then
     logsuffix="$logsuffix\n\nBungeeCord Changes:\n$bungee"
     if [ -z "$updated" ]; then updated="BungeeCord"; else updated="$updated/BungeeCord"; fi
 fi
-disclaimer="Upstream has released updates that appears to apply and compile correctly.\nThis update has not been tested by PaperMC and as with ANY update, please do your own testing"
+if [ ! -z "$waterfall" ]; then
+    logsuffix="$logsuffix\n\nWaterfall Changes:\n$waterfall"
+    if [ -z "$updated" ]; then updated="Waterfall"; else updated="$updated/Waterfall"; fi
+fi
+disclaimer="Upstream has released updates that appears to apply and compile correctly."
 
 if [ ! -z "$1" ]; then
     disclaimer="$@"
